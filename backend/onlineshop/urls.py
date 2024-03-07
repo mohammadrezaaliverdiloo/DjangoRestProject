346997157f381e7dffe82068e30eca2d5911f673
MohammadReza Aliverdiloo <mohammadRezaaliverdiloo@gmail.com>
2024-03-07 20:46:02 +0330

    after start user app

--- backend/onlineshop/urls.py
+++ backend/onlineshop/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("product.urls"),),
+    path('user/',include("user.urls"),),
 ]
 
 if settings.IS_DEVE:
